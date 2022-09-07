5162957320a1df181790dd9042bb7ea37cd117b0
Anitinky13git config --global user.email ayefanova1@gmail.com <ayefanova1@gmail.com>
2022-09-06 21:03:05 -0500

    render indexeddb database data on a card

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,7 +1,7 @@
 i; // Import modules
 import "./form";
 import "./submit";
-import { initdb } from "./database";
+import { initdb, getDb, postDb } from "./database";
 // Import CSS files
 import "../css/index.css";
 //import bootstrap
@@ -11,10 +11,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Logo from "../images/logo.png";
 import Bear from "../images/bear.png";
 import Dog from "../images/dog.png";
-
+//import fetchCards
+import { fetchCards } from "./cards";
 // Add images on load
 window.addEventListener("load", function () {
   initdb();
+  fetchCards();
+
   document.getElementById("logo").src = Logo;
   document.getElementById("bearThumbnail").src = Bear;
   document.getElementById("dogThumbnail").src = Dog;
