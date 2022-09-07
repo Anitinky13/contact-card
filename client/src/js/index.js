d444112f42778052bebda64c1f72d51911c6bdf1
Anitinky13git config --global user.email ayefanova1@gmail.com <ayefanova1@gmail.com>
2022-09-06 21:35:40 -0500

    ready for staging

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,7 +1,7 @@
 i; // Import modules
 import "./form";
 import "./submit";
-import { initdb, getDb, postDb } from "./database";
+import { initdb, getDb, postDb, deleteDb, editDb } from "./database";
 // Import CSS files
 import "../css/index.css";
 //import bootstrap
@@ -13,6 +13,7 @@ import Bear from "../images/bear.png";
 import Dog from "../images/dog.png";
 //import fetchCards
 import { fetchCards } from "./cards";
+import { format } from "path";
 // Add images on load
 window.addEventListener("load", function () {
   initdb();
@@ -22,3 +23,32 @@ window.addEventListener("load", function () {
   document.getElementById("bearThumbnail").src = Bear;
   document.getElementById("dogThumbnail").src = Dog;
 });
+
+window.editCard = (e) => {
+  //Grabs the id from the button element attached to the contact card and sets a global variable that will be used in the form element.
+  profileId = parseInt(e.dataset.id);
+
+  //grabs information to pre-populte edit form
+  let editName = e.dataset.name;
+  let editEmail = e.dataset.email;
+  let editPhone = e.dataset.phone;
+
+  document.getElementById("name").value = editName;
+  document.getElementById("email").value = editEmail;
+  document.getElementById("phone").value = editPhone;
+
+  format.style.display = "block";
+
+  //toggles the Submit button so that it now Updates an existing contact instead of posting a new one
+  submitBtnToUpdate = true;
+};
+// Card functionality
+// Adds deleteCard() to the global scope so each card has access to it.
+window.deleteCard = (e) => {
+  // Grabs the id from the button element attached to the contact card.
+  let id = parseInt(e.id);
+  // Delete the card
+  deleteDb(id);
+  // Reload the DOM
+  fetchCards();
+};
